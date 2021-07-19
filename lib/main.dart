1bd9a5258194a2ae96e1fdab90740e6bff760448
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-19 16:42:02 +0530

    Widget Setup#22

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'pages/home.dart';
+import 'package:world_time/pages/home.dart';
 
 void main() => runApp(MaterialApp(
-  home: ,
+  home: Home(),
 ));
 
