38418b8cc25bbaf4db93811f13b73654c685cac8
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-20 15:20:40 +0530

    #23  Routing with Maps
    ElevatedButton(
                  onPressed: () {
                    Navigator.pushNamed(context, '/location');
                  },
                  child: Text('TextButton'),
                ),

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:worldtime/pages/home.dart';
+// import 'dart:js';
 
-void main() =>  runApp(MaterialApp(
-  home: Home(),
-));
+import 'package:flutter/material.dart';
+import 'package:worldtime/pages/choose_location.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ));
