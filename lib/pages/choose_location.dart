327cbc44702824a370cfd0335ed16f4f4a0dcf38
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-20 15:52:44 +0530

    #24 WIDGET LIFECYCLE | initState() buildState()
      // initState() RUNS ONCE buildState() IS THE ONE KEEPS UPDATING

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,9 +6,24 @@ class ChooseLocation extends StatefulWidget {
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
+
 class _ChooseLocationState extends State<ChooseLocation> {
+  //WIDGET LIFECYCLE
+  // initState() RUNS ONCE buildState() IS THE ONE KEEPS UPDATING
+  int counter=0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('init state Funcation Ran');
+  }
+  //WIDGET LIFECYCLE
+
+
+
   @override
   Widget build(BuildContext context) {
+    print('build Funcation Ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -17,7 +32,20 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose Location Screen'),
+      body: ElevatedButton(
+
+      //WIDGET LIFECYCLE
+
+      child: Text('Counter is $counter'),
+        onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+
+      //WIDGET LIFECYCLE
+
+        },
+      ),
     );
   }
 }
