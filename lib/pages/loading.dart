9f9985a38cbff372eae319ea2b827c9379916c9e
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-20 21:18:31 +0530

    #27 World time API
    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata'));
        Map jsonData= jsonDecode(response.body);
        print('from jsonData $jsonData');

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,13 +13,23 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
   //#26 HTTP
   //Async code represents action that starts now and finishes sometimes in the future
-  void getData() async {
-
-    // Get API Response
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    print('Api Fetch Begins');
+    //Make Api request
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata'));
     Map jsonData= jsonDecode(response.body);
-    print(jsonData);
-    print(jsonData['title']);
+    print('from jsonData $jsonData');
+
+    // get properties from jsonData
+    String datetime = jsonData['utc_datetime'];
+    String offset = jsonData['utc_offset'].substring(1,3);
+    print('From offset $offset, $datetime');
+
+    //Create Date time object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    print ('from now $now');
 
   }
   //#26 HTTP
@@ -30,7 +40,7 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
 
