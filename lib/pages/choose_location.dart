500e37fea52d3b840ee3737729d44a84d7605db3
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-21 12:32:30 +0530

    #34 ListView.Builder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,7 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
 
@@ -9,36 +12,53 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
 
   @override
   Widget build(BuildContext context) {
     print('build Funcation Ran');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose Location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: ElevatedButton(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose Location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
 
-      //WIDGET LIFECYCLE
 
-      // child: Text('Counter is $counter'),
-      child: Text('Counter is'),
+        //ListView.Builder
+        body: ListView.builder(itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: (){
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        } ,)
+      //ListView.Builder
 
-        onPressed: (){
-          setState(() {
-            // counter += 1;
-          });
 
-      //WIDGET LIFECYCLE
 
-        },
-      ),
     );
   }
 }
